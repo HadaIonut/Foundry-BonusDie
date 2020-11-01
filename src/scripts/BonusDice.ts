89af9cf947b62bf0e59622a1d03ef52e0bdcd671
Ionut Hada <hadaionut@gmail.com>
2020-11-01 20:44:17 +0200

    Comments

--- src/scripts/BonusDice.ts
+++ src/scripts/BonusDice.ts
@@ -9,14 +9,28 @@ Hooks.on("init", async () => {
 })
 
 Hooks.on("ready", () => {
+    //event trigger for updating the data un the user side
     game.socket.on('module.BonusDie', (object) => {
         console.log('ceaoa');
         updateCounter($(`#${object.str}`), object.counter);
     });
 })
 
+/**
+ * Updates the counter display
+ *
+ * @param $counter - jQuery element of the span to update
+ * @param newValue - the new value of the span
+ */
 const updateCounter = ($counter, newValue: number) => $counter.text(newValue);
 
+/**
+ * Method called by the buttons to update the numbers displayed
+ *
+ * @param player - owner of the bonus dice
+ * @param modifier - how should the number of bonus dice be modified (+/-)
+ * @param $counterStructure - jQuery obj of the span
+ */
 const modifyBonusDiceAmount = (player: string, modifier: number, $counterStructure) => {
     const counter = getCounter();
     if (isNaN(counter[player])) counter[player] = 0;
@@ -27,10 +41,23 @@ const modifyBonusDiceAmount = (player: string, modifier: number, $counterStructu
     });
 }
 
+/**
+ * Selects if the method above should add or subtract from the counter
+ *
+ * @param type - increase/decrease
+ * @param player - owner of the structure
+ * @param $counterStructure - jQuery obj of the span
+ */
 const methodSelector = (type: string, player: string, $counterStructure) => () => modifyBonusDiceAmount(player, type === "increase" ? 1 : -1, $counterStructure);
 
 const iconSelector = (type: string): string => type === 'increase' ? 'fas fa-plus' : 'fas fa-minus';
 
+/**
+ * Creates the structure for the button
+ *
+ * @param player - owner of the data
+ * @param $counterStructure - jQuery obj of the span
+ */
 const button = (player: string, $counterStructure) => (type: string) => {
     const iconType = iconSelector(type);
     let createdButton = $(`<span style='flex: 0.2'><i class='${iconType}'></i></span>`);
@@ -38,17 +65,39 @@ const button = (player: string, $counterStructure) => (type: string) => {
     return createdButton;
 }
 
-const getBonusDiceValue = (players: string): number => {
+/**
+ * Returns the number of bonus dice held by a player
+ *
+ * @param player
+ */
+const getBonusDiceValue = (player: string): number => {
     const counter = getCounter();
-    if (counter?.[players]) {
-        return counter[players];
+    if (counter?.[player]) {
+        return counter[player];
     } else return 0;
 }
 
+/**
+ * Returns a unique identifier for each span
+ *
+ * @param index - index of the span
+ */
 const getSpanId = (index)=> `BonusDie-${index}`;
 
-const bonusDiceStructure = (players: string, index) => $(`<span id="${getSpanId(index)}" style='flex: 0.1'>${getBonusDiceValue(players)}</i></span>`);
+/**
+ * Creates the structure for the bonus dice display as a span with the number of bonus dice
+ *
+ * @param player - the player owner of the structure
+ * @param index - index of the span
+ */
+const bonusDiceStructure = (player: string, index) => $(`<span id="${getSpanId(index)}" style='flex: 0.1'>${getBonusDiceValue(player)}</i></span>`);
 
+/**
+ * Creates the controls structure for the DM (display, plus button, minus button)
+ *
+ * @param players - player that has it's data controlled
+ * @param index - index of the span
+ */
 const getControls = (players, index) => {
     const $bonusDice = bonusDiceStructure(players.users[index].data._id, index);
 
@@ -63,6 +112,11 @@ const getControls = (players, index) => {
     }
 }
 
+/**
+ * Appends the controls to the players display
+ *
+ * @param players - a list of players
+ */
 const handle = (players) => (index, playerHTML) => $(playerHTML).append(...getControls(players, index));
 
 //Magic one liner, I dont want to talk about this
